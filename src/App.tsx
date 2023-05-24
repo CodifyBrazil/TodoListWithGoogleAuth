9d672dc0de785cf539c5e64268b3103a128f4487
Rafael Guilber <20958795+CodifyBrazil@users.noreply.github.com>
2023-05-23 23:00:31 -0300

    :sparkels: implementation google login

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,58 @@
+import { googleLogout, useGoogleLogin } from '@react-oauth/google'
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+
 function App() {
-  return <h1>Hello</h1>
+  const [user, setUser] = useState([null])
+  const [profile, setProfile] = useState([null])
+
+  const login = useGoogleLogin({
+    onSuccess: (response) => setUser(response),
+    onError: (response) => console.log(response),
+  })
+
+  useEffect(() => {
+    if (user) {
+      axios
+        .get(
+          `https://www.googleapis.com/oauth2/v1/userinfo?access_token=${user.access_token}`,
+          {
+            Headers: {
+              Accept: 'application/json',
+              Authorization: `Bearer ${user.access_token}`,
+            },
+          },
+        )
+        .then((response: any) => setProfile(response.data))
+        .catch((error: any) => console.log(error))
+    }
+  }, [user])
+
+  const logOut = () => {
+    googleLogout()
+    setProfile(null)
+  }
+
+  console.log(profile)
+
+  return (
+    <div>
+      <h2>Google Login</h2>
+      {profile ? (
+        <div>
+          <img src={profile.picture} alt="user image" />
+          <h3>User Logged in</h3>
+          <p>Name: {profile.name}</p>
+          <p>Email Address: {profile.email}</p>
+          <br />
+          <br />
+          <button onClick={logOut}>Log out</button>
+        </div>
+      ) : (
+        <button onClick={() => login()}>Sign in with Google 🚀 </button>
+      )}
+    </div>
+  )
 }
 
 export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
+import { GoogleOAuthProvider } from '@react-oauth/google'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <GoogleOAuthProvider clientId="892236949374-ea62qokf69t4ife2tiveeu8fetenl7nk.apps.googleusercontent.com">
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </GoogleOAuthProvider>,
 )
